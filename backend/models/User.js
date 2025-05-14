e961a57ff49da335b139f4720a8286515d161ca9
Jayanth Ravi <jayanthravi2003@gmail.com>
2025-05-14 20:15:14 +0530

    Replace repo contents with new SHIBI-main folder

--- backend/models/User.js
+++ backend/models/User.js
@@ -73,17 +73,17 @@ userSchema.index({ email: 1 }, { unique: true });
 userSchema.index({ googleId: 1 });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+// userSchema.pre('save', async function(next) {
+//   if (!this.isModified('password')) return next();
   
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 // Method to compare password
 userSchema.methods.comparePassword = async function(candidatePassword) {
